5b68e71f64c0d5c9839b2292533f638bf1c28082
BaquiranCharlesJustin <90014194+BaquiranCharlesJustin@users.noreply.github.com>
2022-10-19 15:31:52 +0800

    Add files via upload

---
+++ demo4.php
@@ -0,0 +1,34 @@
+<html>
+<head><title> Bardagulan</title></head>
+<body>
+<h1 align="center"> Welcome to WDD </h1>
+<?php
+	
+	for ($fuel=-1; $fuel <10; ++$fuel)
+		
+		echo "Hello". $fuel + 1, "<br>";
+		
+	//$colors = array("red","green","blue","yellow","white","pink");
+		//foreach ($colors as $value)
+		//{
+		//	echo "$value <br>";
+		//}
+		//for ($x = 0; $x < 10; $x++)
+		//{
+		//	if($x==4)
+		//	{
+		//		break;
+		//	}
+		//		echo "The number is : $x <br>";
+	//	}
+	//for ($count = 0; $x <= 10; $x++)
+		//{
+		//	if($x==0)
+		//	{
+			//	continue;
+		//	}
+			//	echo "Hello $x <br>";
+		//}
+?>
+</body>
+</html>
